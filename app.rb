25470804b0ce8eb175f463a2a4d9645a51c16f1d
Victoria Mei <vm930@hotmail.com>
2019-03-10 23:08:00 -0400

    1st sinatra lab done

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/name' do 
+        "My name is victoria"
+    end
+
+    get '/hometown' do 
+        "My hometown is China"
+    end
+
+    get '/favorite-song' do 
+        "My favorite song is so bit it"
+    end
 end
